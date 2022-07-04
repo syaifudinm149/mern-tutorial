38ec713bf7af140f455d424aef76011552d12407
Muhammad Syaifudin <syaifudinm84@gmail.com>
2022-07-05 00:19:44 +0700

    membuat Route

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+// console.log("hello world")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+app.get('/api/goals', (req, res) => {
+  res.send({ message: 'Get Goals' })
+})
+
+app.use('/api/goals', require('./routes/goalRoutes.js'))
+
+app.listen(port, () => console.log(`server started on port ${port}`))
